4cf8e06292ce3726bad1a1928f84d5dbf0b849f7
Valério <vnrj@ic.ufal.br>
2022-12-04 14:44:23 -0300

    Adiciona simulação

--- force_control/main.py
+++ force_control/main.py
@@ -1,27 +1,31 @@
+from pathlib import Path
+
 from matplotlib import pyplot
 import numpy
 
 from zmqRemoteApi import RemoteAPIClient
 
-buffer_len = 100
+buffer_len = 10
 
 
 def main():
     client = RemoteAPIClient()
+    client.setStepping(True)
+
+    time_span = numpy.arange(0, buffer_len)
+
     sim = client.getObject('sim')
+    sim.loadScene(str(Path('solid_simple.ttt').absolute()).replace('\\', '/'))
 
     joints = [sim.getObjectHandle(f'theta{i + 1}') for i in range(5)]
     buffer = numpy.zeros((len(joints), buffer_len))
-    time_span = numpy.arange(0, buffer_len)
-    t = 0
-
-    client.setStepping(True)
-    print('Starting simulation...')
-    sim.startSimulation()
     fig, ax = pyplot.subplots(1, len(joints), figsize=(5 * len(joints), 2))
 
+    sim.startSimulation()
+
     try:
-        while pyplot.fignum_exists(fig.number):
+        t = 0
+        while t < buffer_len:
             client.step()
 
             buffer[:, t] = [sim.getJointForce(joint) for joint in joints]
@@ -37,7 +41,24 @@ def main():
             pyplot.pause(0.0001)
 
             t += 1
-            t = t % buffer_len
+    finally:
+        sim.stopSimulation()
+
+    pyplot.close(fig)
+    while sim.getSimulationState() != sim.simulation_stopped:
+        pass
+    sim.loadScene(str(Path('solid_force.ttt').absolute()).replace('\\', '/'))
+    sim.startSimulation()
+
+    try:
+        while True:
+
+            for i, joint in enumerate(joints):
+                print(f'Setting joint{i + 1} force to {buffer[i, -1]}...')
+                sim.setJointForce(joint, 2 * buffer[i, -1])
+                print(f'Read joint{i + 1} force: {sim.getJointForce(joint)} target: {sim.getJointTargetForce(joint)}')
+
+            client.step()
     finally:
         sim.stopSimulation()
 
